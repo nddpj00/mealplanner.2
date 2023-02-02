0b2419690c07de4b48ad4e229872d35a1e9975f5
nddpj00 <danieljones625@btinternet.com>
2023-02-02 16:36:27 +0000

    adds add category page directed from add category button on categories page

--- mealplanner/routes.py
+++ mealplanner/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from mealplanner import app, db
 from mealplanner.models import Category, Recipe
 
@@ -6,3 +6,16 @@ from mealplanner.models import Category, Recipe
 @app.route("/")
 def home():
     return render_template("categories.html")
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(food_category=request.form.get("food_category"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
