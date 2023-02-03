81d9720127bb5251cf6c74acabebca215fbf2aab
nddpj00 <danieljones625@btinternet.com>
2023-02-03 15:00:04 +0000

    adds form to add_recipe page

--- mealplanner/routes.py
+++ mealplanner/routes.py
@@ -11,11 +11,23 @@ def home():
 def categories():
     return render_template("categories.html")
 
-@app.route("/add_category", methods=["GET", "POST"])
-def add_category():
+
+@app.route("/add_recipe", methods=["GET", "POST"])
+def add_recipe():
+    categories = list(Category.query.order_by(Category.food_category).all())
     if request.method == "POST":
-        category = Category(food_category=request.form.get("food_category"))
-        db.session.add(category)
+        recipe = Recipe(
+            recipe_name=request.form.get("recipe_name"),
+            recipe_notes=request.form.get("recipe_notes"),
+            recipe_cuisine=request.form.get("recipe_cuisine"),
+            cook_time=request.form.get("cook_time"),
+            recipe_location=request.form.get("recipe_location"),
+            family_friendly=bool(True if request.form.get("family_friendly") else False),
+            recipe_healthy=bool(True if request.form.get("recipe_healthy") else False),
+            date_added=request.form.get("date_added"),
+            category_id=request.form.get("category_id")
+        )
+        db.session.add(recipe)
         db.session.commit()
-        return redirect(url_for("categories"))
-    return render_template("add_category.html")
+        return redirect(url_for("home"))
+    return render_template("add_recipe.html", categories=categories)
