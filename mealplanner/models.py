c5dc5fa3e1368e6fc6842a3c3f26e25156911495
nddpj00 <danieljones625@btinternet.com>
2023-02-20 19:39:00 +0000

    adds cuisine table and for loop for add recipe form

--- mealplanner/models.py
+++ mealplanner/models.py
@@ -11,22 +11,32 @@ class Category(db.Model):
         # __repr__ to represent itself in the form of a string
         return self.food_category
 
+class Cuisine(db.Model):
+    # schema for the Category model
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_cuisine = db.Column(db.String(25), unique=True, nullable=False)
+    recipes = db.relationship("Recipe", backref="cuisine", cascade="all, delete", lazy=True)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.recipe_cuisine
+
 
 class Recipe(db.Model):
     # schema for the Recipe model
     id = db.Column(db.Integer, primary_key=True)
     recipe_name = db.Column(db.String(50), unique=True, nullable=False)
     recipe_notes = db.Column(db.Text, nullable=False)
-    recipe_cuisine = db.Column(db.String, nullable=False)
     cook_time = db.Column(db.Integer, nullable=False)
     recipe_location = db.Column(db.Text, nullable=False)
     family_friendly = db.Column(db.Boolean, default=False, nullable=False)
     recipe_healthy = db.Column(db.Boolean, default=False, nullable=False)
     date_added = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE", onupdate= "CASCADE"), nullable=False)
+    cuisine_id = db.Column(db.Integer, db.ForeignKey("cuisine.id", ondelete="CASCADE", onupdate= "CASCADE"), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return "#{0} - Recipe: {1} | Cook: {2} mins".format(
-            self.id, self.recipe_name, self.cook_time
+        self.id, self.recipe_name, self.cook_time
         )
--- mealplanner/routes.py
+++ mealplanner/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from mealplanner import app, db
-from mealplanner.models import Category, Recipe
+from mealplanner.models import Category, Recipe, Cuisine
 
 
 @app.route("/")
@@ -15,19 +15,24 @@ def categories():
 @app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
     categories = list(Category.query.order_by(Category.food_category).all())
+    cuisines = list(Cuisine.query.order_by(Cuisine.recipe_cuisine).all())
     if request.method == "POST":
         recipe = Recipe(
             recipe_name=request.form.get("recipe_name"),
             recipe_notes=request.form.get("recipe_notes"),
-            recipe_cuisine=request.form.get("recipe_cuisine"),
             cook_time=request.form.get("cook_time"),
             recipe_location=request.form.get("recipe_location"),
             family_friendly=bool(True if request.form.get("family_friendly") else False),
             recipe_healthy=bool(True if request.form.get("recipe_healthy") else False),
             date_added=request.form.get("date_added"),
-            category_id=request.form.get("category_id")
+            category_id=request.form.get("category_id"),
+            cuisine_id=request.form.get("cuisine_id")
         )
         db.session.add(recipe)
         db.session.commit()
         return redirect(url_for("home"))
-    return render_template("add_recipe.html", categories=categories)
+    return render_template("add_recipe.html", categories=categories, cuisines=cuisines)
+
+
+"""@app.route("/oily-fish", methods=["GET", "POST"])
+def oily_fish():"""
