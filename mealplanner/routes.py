8c285abf3a9a80a09fb961c67c48a865a57ff88f
nddpj00 <danieljones625@btinternet.com>
2023-02-22 21:58:48 +0000

    adds info to category page cards and updated button links to individual category recipe pages

--- mealplanner/routes.py
+++ mealplanner/routes.py
@@ -35,7 +35,27 @@ def add_recipe():
     return render_template("add_recipe.html", categories=categories, cuisines=cuisines)
 
 
-@app.route("/oily-fish", methods=["GET", "POST"])
+@app.route("/vegetarian", methods=["GET"])
+def vegetarian():
+    veg_recipe = list(Recipe.query.filter_by(category_id=1).all())
+    return render_template("vegetarian.html", veg_recipe=veg_recipe)
+
+@app.route("/white_meat", methods=["GET"])
+def white_meat():
+    wmeat_recipe = list(Recipe.query.filter_by(category_id=2).all())
+    return render_template("white_meat.html", wmeat_recipe=wmeat_recipe)
+
+@app.route("/red_meat", methods=["GET"])
+def red_meat():
+    rmeat_recipe = list(Recipe.query.filter_by(category_id=3).all())
+    return render_template("red_meat.html", rmeat_recipe=rmeat_recipe)
+
+@app.route("/oily-fish", methods=["GET"])
 def oily_fish():
-    ofish_recipe = list(Recipe.query.order_by(Recipe.id).all())
+    ofish_recipe = list(Recipe.query.filter_by(category_id=4).all())
     return render_template("oily_fish.html", ofish_recipe=ofish_recipe)
+
+@app.route("/white-fish", methods=["GET"])
+def white_fish():
+    wfish_recipe = list(Recipe.query.filter_by(category_id=5).all())
+    return render_template("white_fish.html", wfish_recipe=wfish_recipe)
