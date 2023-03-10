1b531e6b39df5eccd0965470f86130f7c2466801
nddpj00 <danieljones625@btinternet.com>
2023-03-10 14:12:06 +0000

    moves set random_vegmeal out of btn onclick to attempt to fix bug

--- mealplanner/routes.py
+++ mealplanner/routes.py
@@ -15,7 +15,8 @@ def home():
     rmeat_recipe = list(Recipe.query.filter_by(category_id=3).all())
     ofish_recipe = list(Recipe.query.filter_by(category_id=4).all())
     wfish_recipe = list(Recipe.query.filter_by(category_id=5).all())
-    return render_template("categories.html", veg_recipe=veg_recipe, wmeat_recipe=wmeat_recipe, rmeat_recipe=rmeat_recipe, ofish_recipe=ofish_recipe, wfish_recipe=wfish_recipe )
+    return render_template("categories.html", veg_recipe=veg_recipe, wmeat_recipe=wmeat_recipe, 
+    rmeat_recipe=rmeat_recipe, ofish_recipe=ofish_recipe, wfish_recipe=wfish_recipe )
 
 
 @app.route("/add_recipe", methods=["GET", "POST"])
