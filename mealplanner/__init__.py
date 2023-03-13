225483a92612169a98ed5fba67a494066c486644
nddpj00 <danieljones625@btinternet.com>
2023-03-13 10:36:13 +0000

    adds cuisines to database

--- mealplanner/__init__.py
+++ mealplanner/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 if os.path.exists("env.py"):
     import env  # noqa
 
@@ -17,5 +18,6 @@ else:
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from mealplanner import routes  # noqa d
